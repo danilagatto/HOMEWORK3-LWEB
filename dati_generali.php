377b07fe5ff8cf83f27846ef529c79ab850b2eda
danilagatto <gatto.2075528@studenti.uniroma1.it>
2026-08-01 22:09:29 +0200

    Add files via upload

---
+++ dati_generali.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$utente = "root";
+$password = "";
+
+$nome_database = "danila.gatto.XML-DOM";
+
+$tabella_cliente = "cliente";
+$tabella_viaggio = "viaggio";
+$tabella_prenotazione = "prenotazione";
+$tabella_pagamento = "pagamento";
+
+$tabella_bali = "bali";
+$tabella_kyoto = "kyoto";
+$tabella_reykjavik = "reykjavik";
+$tabella_losangeles = "losangeles";
